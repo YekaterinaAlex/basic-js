8b36a46fa7f4f1ba11bb679b56cc6423e420a806
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2025-10-22 10:11:35 -0700

    feat: add depthCalc

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) return 0;
+    if (arr.length === 0) return 1;
+    let maxDepth = 1;
+    for (let el of arr) {
+      if (Array.isArray(el)) {
+        const depth = this.calculateDepth(el) + 1;
+        if (depth > maxDepth) {
+          maxDepth = depth;
+        }
+      }
+    }
+    return maxDepth;
   }
 }
 
