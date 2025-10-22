0554242465fd39211c650950850997f4d7a237da
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2025-10-22 11:59:28 -0700

    feat:add common char count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,11 +12,21 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const charS1 = [...s1].reduce(
+    (acc, char) => ((acc[char] = (acc[char] || 0) + 1), acc),
+    {}
+  );
+  const charS2 = [...s2].reduce(
+    (acc, char) => ((acc[char] = (acc[char] || 0) + 1), acc),
+    {}
+  );
+  return Object.keys(charS1).reduce(
+    (sum, char) => sum + Math.min(charS1[char], charS2[char] || 0),
+    0
+  );
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
